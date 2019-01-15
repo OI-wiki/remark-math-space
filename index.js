1d1e29c761290dd58e712a301c272b9def201b68
Ir1d <sirius.caffrey@gmail.com>
2019-01-15 19:28:36 +0800

    fix: also add space for inlineCode

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ function gap() {
   var Compiler = this.Compiler;
   var visitors = Compiler.prototype.visitors;
   var original = visitors.inlineMath;
+  var originalInlineCode = visitors.inlineCode;
 
+  visitors.inlineCode = inlineCode;
   visitors.inlineMath = inlineMath;
 
   function inlineMath(node) {
     return " " + original.apply(this, arguments) + " ";
+  }
+  function inlineCode(node) {
+    return " " + originalInlineCode.apply(this, arguments) + " ";
   }
 }
