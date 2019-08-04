ef1da24fd74f14e73a59250a650868adcdf4e0da
Ir1d <sirius.caffrey@gmail.com>
2019-08-04 21:56:35 +0800

    add space for strong and link

--- index.js
+++ index.js
@@ -5,14 +5,24 @@ function gap() {
   var visitors = Compiler.prototype.visitors;
   var original = visitors.inlineMath;
   var originalInlineCode = visitors.inlineCode;
+  var originalStrong = visitors.strong;
+  var originalLink = visitors.link;
 
   visitors.inlineCode = inlineCode;
+  visitors.strong = strong;
   visitors.inlineMath = inlineMath;
+  visitors.link = link;
 
   function inlineMath(node) {
     return " " + original.apply(this, arguments) + " ";
   }
   function inlineCode(node) {
     return " " + originalInlineCode.apply(this, arguments) + " ";
+  }
+  function strong(node) {
+    return " " + originalStrong.apply(this, arguments) + " ";
+  }
+  function link(node) {
+    return " " + originalLink.apply(this, arguments) + " ";
   }
 }
